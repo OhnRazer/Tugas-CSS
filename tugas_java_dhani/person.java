074452d9c68962e952d476303e5684a1373493a0
Ramdhani <Anonymousramdhani@gmail.com>
2013-09-20 10:34:21 +0700

    sesi3

---
+++ tugas_java_dhani/person.java
@@ -0,0 +1,9 @@
+class person {
+public static void main (String[] abc){
+	int x = 10;
+	char y;
+	y='A';
+	System.out.println("menampilkan variabel x="+x);
+	System.out.println("menampilkan variabel y="+y);
+	}
+}
